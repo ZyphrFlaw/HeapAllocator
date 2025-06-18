8fe81e9a8bede9a98e9e6b8c7f5d0249d69fa15c
Flaw-Zyphr <forsaken250.com>
2025-06-19 00:35:03 +0530

    Added block splitting and free_list logic

--- main.c
+++ main.c
@@ -5,13 +5,15 @@
 #include <inttypes.h>
 #include <sys/mman.h>
 
-#define PAGE_SIZE 4096
+#define PAGE_SIZE sysconf(_SC_PAGESIZE)
 
 typedef struct node
 {
     size_t size;
     struct node* next;
 }header_t;
+
+header_t* free_list = NULL;
 
 void* myMalloc(size_t size)
 {
@@ -21,13 +23,39 @@ void* myMalloc(size_t size)
         return NULL;
     }
 
-    size_t required_size = (size + PAGE_SIZE)/PAGE_SIZE * PAGE_SIZE; //Round-off to nearest PAGE_SIZE
+    size_t required_size = (size + PAGE_SIZE + sizeof(header_t) - 1)/PAGE_SIZE * PAGE_SIZE; //Round-off to nearest PAGE_SIZE
+    printf("Requested size = %zu\n", required_size);
+    header_t* prev = NULL;
+    header_t* curr = free_list;
+    while(curr != NULL)
+    {
+        printf("curr Size = %zu\n", curr->size);
+        if (required_size <= curr->size)
+        {
+            printf("In realloc()\n");
+            header_t* allocated_block = curr;
+            header_t* new_block = (header_t*)((char*)curr + required_size);
+            new_block->size = curr->size - required_size;
+            new_block->next = curr->next;
+            if (prev == NULL)
+                free_list = new_block;
+            else
+                prev->next = new_block;
+            return (void*)(allocated_block + 1);
+        }
+        prev = curr;
+        curr = curr->next;
+    }
     void* add = mmap(NULL, required_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    if ( add == MAP_FAILED )
+    {
+        printf("Error mmp() Failed to allocate memory");
+        return NULL;
+    }
     header_t* header = (header_t*)add;
     header->size = required_size;
     header->next = (void*)(0x01);
-
-    return header;
+    return(void*)(header + 1);
 }
 
 void myFree(void* add)
@@ -38,8 +66,9 @@ void myFree(void* add)
        return;
    }
 
-   header_t* header = (header_t*)add;
-   if (header->next == (void*)(0x01))
+   header_t* header = (header_t*)add - 1;
+   printf("find = %p\n", header->next);
+   if (header->next == (void*)(0x01) && header->size > 4096509)
    {
        size_t free_size = header->size;
        int ret = munmap(header, free_size);
@@ -51,20 +80,45 @@ void myFree(void* add)
 
        printf("Successfully freed the memory Address %p\n", add);
    }
+   else
+   {
+       if(free_list != NULL)
+       {
+            header_t* temp = free_list;
+            free_list = (header_t*)(header);
+            free_list->size = header->size;
+            free_list->next = temp;
+       }
+       else
+       {
+            free_list = (header_t*)(header);
+            free_list->size = header->size;
+            free_list->next = NULL;
+       }
+   }
    return;
 }
 
 int main()
 {
-    size_t request_size = 10;
-    void* add = myMalloc(request_size);
-    if (add == NULL)
+    size_t request_size = 10000;
+    for (int i = 0;i<2;i++)
     {
-        printf("Error allocating memory in myMalloc()\n");
-        return -1;
+        int* add = (int*)myMalloc(request_size);
+        printf("New Memory Address = %p\n", add);
+        request_size += 10;
+        if (add == NULL)
+        {
+            printf("Error allocating memory in myMalloc()\n");
+            return -1;
+        }
+        myFree(add);
+        request_size = 10;
     }
-    printf("Address = %p\n", add);
-
-    myFree(add);
+    while(free_list != NULL)
+    {
+        printf("Address = %p and Size = %zu\n", free_list, free_list->size);
+        free_list = free_list->next;
+    }
     return 0;
 }
